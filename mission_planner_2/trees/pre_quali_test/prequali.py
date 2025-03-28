85ce738cb5b4926b513ba107928b77d4daa4493e
bbauv3 <bumblebeeauv@gmail.com>
2025-03-29 00:39:50 +0800

    can help to push the tree if got time

---
+++ mission_planner_2/trees/pre_quali_test/prequali.py
@@ -0,0 +1,233 @@
+import csv
+import operator
+
+import py_trees
+import py_trees_ros.trees
+from bb_auv_msgs.srv import ResetPose
+from bb_controls_msgs.action import Locomotion
+from bb_controls_msgs.srv import Controller
+
+from mission_planner_2 import service_clients
+
+WAYPOINTS = []
+OFFSET = []
+CSV_FILEPATH = (
+    "/home/bbauv4/workspaces/ros2_ws/src/controls_tests/controls_tests/quali.csv"
+)
+
+
+def _gen_goal(idx):
+    goal_msg = Locomotion.Goal()
+
+    # Set the required fields
+    goal_msg.move_rel = False
+    goal_msg.depth_rel = False
+    goal_msg.heading_rel = False
+    try:
+        goal_msg.depth_ctrl = Locomotion.Goal.DEPTH_MODE_DEPTH
+    except Exception as e:
+        print(e)
+        goal_msg.depth_ctrl = 0
+    goal_msg.specified_heading = True
+
+    # Set the setpoints
+    goal_msg.forward_setpoints = [WAYPOINTS[idx][0] + OFFSET[0]]
+    goal_msg.sidemove_setpoints = [WAYPOINTS[idx][1] + OFFSET[1]]
+    goal_msg.depth_setpoints = [WAYPOINTS[idx][2] + OFFSET[2]]
+    goal_msg.heading_setpoints = [WAYPOINTS[idx][3] + OFFSET[3]]
+
+    # Lists that need to be populated but aren't used
+    goal_msg.roll_setpoints = [0.0]
+    goal_msg.pitch_setpoints = [0.0]
+    goal_msg.altitude_setpoints = []
+
+    return goal_msg
+
+
+class DynamicSetBlackboard(py_trees.behaviour.Behaviour):
+    def __init__(
+        self,
+        name,
+        key,
+        update_key,
+        overwrite=False,
+        func=lambda x: x,
+    ):
+        super().__init__(name)
+        self.key = key
+        self.update_key = update_key
+        self.func = func
+        self.overwrite = overwrite
+        self.blackboard = self.attach_blackboard_client(name="updater")
+        self.blackboard.register_key(
+            key=self.update_key,
+            access=py_trees.common.Access.WRITE,
+        )
+        self.blackboard.register_key(
+            key=self.key,
+            access=py_trees.common.Access.READ,
+        )
+
+    # TODO: write the initialise to validate the blackboard
+
+    def update(self) -> py_trees.common.Status:
+        curr = self.blackboard.get(self.key)
+        if self.blackboard.set(
+            name=self.update_key,
+            value=self.func(curr),
+            overwrite=self.overwrite,
+        ):
+            return py_trees.common.Status.SUCCESS
+
+        return py_trees.common.Status.FAILURE
+
+
+def _set_waypoints(csv_filepath):
+    """
+    Load waypoints from a CSV file.
+    The first line is the offset, subsequent lines are waypoints.
+
+    Args:
+        csv_filepath: Path to the CSV file
+    """
+    global OFFSET, WAYPOINTS
+
+    try:
+        with open(csv_filepath, "r") as csvfile:
+            reader = csv.reader(csvfile)
+            for i, row in enumerate(reader):
+                if len(row) != 4:
+                    continue
+
+                try:
+                    x, y, z, yaw = map(float, row)
+                    WAYPOINTS.append((x, y, z, yaw))
+                except ValueError as e:
+                    continue
+    except Exception as e:
+        return
+
+    if not WAYPOINTS:
+        return
+
+    # Extract the offset (first line)
+    OFFSET = WAYPOINTS[0]
+
+    # remove the offset from the waypoints
+    WAYPOINTS = WAYPOINTS[1:]
+
+
+_set_waypoints(CSV_FILEPATH)
+
+
+def create_pre_qual_root() -> py_trees.common.Status:
+    reset_req = ResetPose.Request()
+    reset_req.reset = True
+
+    controller_req = Controller.Request()
+    controller_req.enable = True
+    controller_req.disable_altitude = False
+    controller_req.pause = False
+
+    main_seq = py_trees.composites.Sequence(
+        name="main_seq",
+        memory=True,
+    )
+
+    reset_pose_client = service_clients.FromConstant(
+        name="reset_pose",
+        service_type=ResetPose,
+        service_name="/auv4/nav/reset_pose",
+        service_request=reset_req,
+    )
+
+    enable_controller_client = service_clients.FromConstant(
+        name="enable_controller",
+        service_type=Controller,
+        service_name="/auv4/controls/controller",
+        service_request=controller_req,
+    )
+
+    movement_selector = py_trees.composites.Selector(
+        name="movement_selector",
+        memory=True,
+    )
+
+    init_idx = py_trees.behaviours.SetBlackboardVariable(
+        name="set_waypoint_idx",
+        variable_name="idx",
+        variable_value=0,
+        overwrite=True,
+    )
+
+    is_waypoints_done = py_trees.behaviours.CheckBlackboardVariableValue(
+        name="check_idx_done",
+        check=py_trees.common.ComparisonExpression(
+            variable="idx",
+            operator=operator.ge,
+            value=len(WAYPOINTS),
+        ),
+    )
+
+    movement_seq = py_trees.composites.Sequence(
+        name="movement_seq",
+        memory=True,
+    )
+
+    idx_updater = DynamicSetBlackboard(
+        name="idx_updater",
+        key="idx",
+        update_key="idx",
+        func=lambda i: i + 1,
+        overwrite=True,
+    )
+
+    waypoint_updater = DynamicSetBlackboard(
+        name="waypoint_updater",
+        key="idx",
+        update_key="waypoint_goal",
+        func=lambda i: _gen_goal(i),
+        overwrite=True,
+    )
+
+    send_action_goal = py_trees_ros.action_clients.FromBlackboard(
+        name="action_client",
+        action_type=Locomotion,
+        action_name="/auv4/controls",
+        key="waypoint_goal",
+    )
+
+    init_seq = py_trees.composites.Sequence(
+        name="init",
+        memory=True,
+        children=[reset_pose_client, init_idx, enable_controller_client],
+    )
+
+    movement_seq.add_children([waypoint_updater, send_action_goal, idx_updater])
+    movement_selector.add_children(
+        [
+            is_waypoints_done,
+            py_trees.decorators.Inverter(
+                name="invert_movement_stat", child=movement_seq
+            ),
+        ]
+    )
+
+    main_seq.add_children(
+        [
+            init_seq,
+            # py_trees.decorators.OneShot(
+            #     name="weee",
+            #     child=init_seq,
+            #     policy=py_trees.common.OneShotPolicy.ON_SUCCESSFUL_COMPLETION,
+            # ),
+            # movement_selector,
+            py_trees.decorators.Retry(
+                name="retry_until_success",
+                child=movement_selector,
+                num_failures=len(WAYPOINTS),
+            ),
+        ]
+    )
+
+    return main_seq
--- scripts/main.py
+++ scripts/main.py
@@ -2,36 +2,37 @@
 import py_trees
 import py_trees.console as console
 import py_trees_ros.trees
-
 import rclpy
 
+from mission_planner_2.trees.pre_quali_test.prequali import create_pre_qual_root
 from mission_planner_2.trees.turtlesim.turtle_circle import create_turtle_circle_root
+
 
 def main():
     rclpy.init(args=None)
-    root = create_turtle_circle_root()
+    root = create_pre_qual_root()
     py_trees.logging.level = py_trees.logging.Level.DEBUG
-    tree = py_trees_ros.trees.BehaviourTree(
-        root=root,
-        unicode_tree_debug=True
-    )
+    tree = py_trees_ros.trees.BehaviourTree(root=root, unicode_tree_debug=True)
     try:
         tree.setup(timeout=15.0)
     except:
         console.logerror(console.red + "failed to setup the tree")
         tree.shutdown()
         rclpy.shutdown()
+
     def stop_on_success(tree):
         if tree.root.status == py_trees.common.Status.SUCCESS:
             console.loginfo(console.green + "completed one execution")
             tree.shutdown()
             rclpy.shutdown()
+
     tree.add_post_tick_handler(stop_on_success)
     tree.tick_tock(period_ms=100)
     try:
         rclpy.spin(tree.node)
     except KeyboardInterrupt:
         pass
+
 
 if __name__ == "__main__":
     main()
