900af4e12339ebed74a20cd417bff5bd5b94d295
wesley <wesleywong08@gmail.com>
2025-05-13 13:31:09 +0000

    feat: initial tree for torp task
    - missing move to task implementation
    - missing launch torp pub

---
+++ mission_planner_2/trees/auv/torpedo/torpedo.py
@@ -0,0 +1,143 @@
+import operator
+
+import py_trees
+import py_trees_ros
+from bb_msgs.srv import IMPoseEstimatorToggleTemplate
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
+
+from mission_planner_2.dynamic_set_blackboard import DynamicSetBlackboard
+from mission_planner_2.trees.auv.goto import goto_node
+from mission_planner_2.trees.auv.torpedo.move_to_task import create_move_to_task_root
+
+
+def _gen_enable_req():
+    """
+    Generate the service for the torpedo tree.
+    """
+    req = IMPoseEstimatorToggleTemplate.Request()
+    req.enabled = True
+    req.template_name = "torpedo"
+    return req
+
+
+def _gen_disable_req():
+    """
+    Generate the service for the torpedo tree.
+    """
+    req = IMPoseEstimatorToggleTemplate.Request()
+    req.enabled = False
+    return req
+
+
+def _convert_pose(pose: PoseWithCovarianceStamped):
+    """
+    Convert the PoseWithCovarianceStamped message to a PoseStamped message.
+    Adds the offset to the holes too.
+
+    TODO: figure out the offset calculation laze for now.
+    """
+    pose_stamped = PoseStamped()
+    pose_stamped.header = pose.header
+    pose_stamped.pose = pose.pose.pose
+    return pose_stamped
+
+
+def create_torpedo_root():
+    """
+    Create the root of the torpedo tree.
+    """
+    TOGGLE_TEMPLATE_TOPIC = "/auv4/image_matching/toggle_template"
+
+    root = py_trees.composites.Sequence(
+        name="Torpedo Root",
+        memory=True,
+    )
+
+    launch_seq = py_trees.composites.Sequence(
+        name="Launch Torpedo",
+        memory=True,
+    )
+
+    # contains the logic for launching the torpedo
+
+    # 1 - move to task - in progress
+    # 2 - enable detections + align to target
+    # 3 - launch torpedo
+    # 4 - disable detections
+
+    enable_detections = py_trees_ros.service_clients.FromConstant(
+        name="Enable Detections",
+        service_name=TOGGLE_TEMPLATE_TOPIC,
+        service_type=IMPoseEstimatorToggleTemplate,
+        service_request=_gen_enable_req(),
+        key_response="torpedo_enable_detections",
+    )
+
+    disable_detections = py_trees_ros.service_clients.FromConstant(
+        name="Disable Detections",
+        service_name=TOGGLE_TEMPLATE_TOPIC,
+        service_type=IMPoseEstimatorToggleTemplate,
+        service_request=_gen_disable_req(),
+        key_response="torpedo_disable_detections",
+    )
+
+    # check srv call succeeded from the BB
+    enable_detections_succeeded = py_trees.behaviours.CheckBlackboardVariableValue(
+        name="Enable Detections Succeeded",
+        check=py_trees.common.ComparisonExpression(
+            variable="torpedo_enable_detections",
+            value=True,
+            operator=lambda x, y: operator.eq(x, y),
+        ),
+    )
+
+    disable_detections_succeeded = py_trees.behaviours.CheckBlackboardVariableValue(
+        name="Disable Detections Succeeded",
+        check=py_trees.common.ComparisonExpression(
+            variable="torpedo_disable_detections",
+            value=True,
+            operator=lambda x, y: operator.eq(x, y),
+        ),
+    )
+
+    pose_sub = py_trees_ros.subscribers.ToBlackboard(
+        name="Pose Subscriber",
+        topic_name="/auv4/front_cam/image_matching/pose",
+        topic_type=PoseWithCovarianceStamped,
+        qos_profile=1,
+        blackboard_variables={"torpedo_pose": None},
+    )
+
+    convert_pose = DynamicSetBlackboard(
+        name="Convert Pose",
+        key="torpedo_pose",
+        update_key="torpedo_pose",
+        overwrite=True,
+        func=_convert_pose,
+    )
+
+    align_to_target = goto_node.FromBlackboard(
+        name="Align to Target",
+        pose_key="torpedo_pose",
+    )
+
+    launch_seq.add_children(
+        children=[
+            enable_detections,
+            enable_detections_succeeded,
+            pose_sub,
+            convert_pose,
+            align_to_target,
+            disable_detections,
+            disable_detections_succeeded,
+        ],
+    )
+
+    root.add_children(
+        children=[
+            create_move_to_task_root(),
+            launch_seq,
+        ]
+    )
+
+    return root
