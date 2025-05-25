53a9e63856e83d083e4d58b4715fe54b57c04a23
YosieSYX <e1300499@u.nus.edu>
2025-05-25 08:51:59 +0800

    Update bin tree

--- mission_planner_2/trees/auv/bins/bins.py
+++ mission_planner_2/trees/auv/bins/bins.py
@@ -5,17 +5,81 @@ import py_trees_ros
 from bb_msgs.srv import IMPoseEstimatorToggleTemplate
 from rclpy.qos import qos_profile_system_default
 from std_msgs.msg import UInt8
+import std_srvs
 
-from mission_planner_2.commons.blackboard import full_key_generator
+from mission_planner_2.commons.blackboard import (
+    DynamicSetBlackboard,
+    full_key_generator
+)
 from mission_planner_2.commons.namespace_utils import generate_namespace
 from mission_planner_2.commons.pose_utils import create_stamped_pose
 from mission_planner_2.trees.auv.goto import goto_node
-from mission_planner_2.trees.auv.bin.move_to_task import create_move_to_bin_task_root
+from mission_planner_2.trees.auv.bins.move_to_task import create_move_to_bin_task_root
 
 
 NAMESPACE = generate_namespace()
 fk = full_key_generator(NAMESPACE)
 
+# For Yosie
+
+    # subscriber --> yaw (NED)
+
+    # rotation_pose = create_stamped_pose(
+    #     "auv4/base_link_ned",
+    #     0.0,  # Temporary, please update
+    #     0.0,
+    #     0.0,
+    #     0.0,
+    #     0.0,
+    #     yaw,
+    # )
+
+    # goto to rotation pose
+
+    # service call to /auv4/choice std_srvs::Trigger (py_trees API tells you how to do this)
+
+    # choose between offsets based on choice (see how torpedo does it)
+
+    # goto that offset
+    
+     # Figure out the offset
+
+TEMPLATE_3_OFFSET_SHARK = {
+    "x": 0.0,
+    "y": 0.0,
+    "z": 0.0,
+    "roll": 0.0,
+    "pitch": 0.0,
+    "yaw": 0.0,
+}
+
+TEMPLATE_3_OFFSET_FISH = {
+    "x": 0.0,
+    "y": 0.0,
+    "z": 0.0,
+    "roll": 0.0,
+    "pitch": 0.0,
+    "yaw": 0.0,
+}
+
+def _make_selection(choice: bool):
+    """
+    Function to set the offset based on choice.
+    """
+    if choice == True:
+        offset = TEMPLATE_3_OFFSET_SHARK
+    else:  # fish
+        offset = TEMPLATE_3_OFFSET_FISH
+
+    return create_stamped_pose(
+        "Task03_DropBRUVS_optical/clustered",
+        position_x=offset["x"],
+        position_y=offset["y"],
+        position_z=offset["z"],
+        roll=offset["roll"],
+        pitch=offset["pitch"],
+        yaw=offset["yaw"],
+    )
 
 def create_bin_root():
     """
@@ -36,9 +100,11 @@ def create_bin_root():
     # contains the logic for dropping BRUVS into the bin
 
     # 1 - move to task - in progress
-    # 2 - enable detections + align to target
-    # 3 - drop into bin twice
-    # 4 - disable detections
+    # 2 - enable detections 
+    # 3 - service call to obtain choice
+    # 4 - align to target
+    # 5 - drop into bin twice
+    # 6 - disable detections
 
     enable_detections = py_trees_ros.service_clients.FromConstant(
         name="Enable Detections",
@@ -79,33 +145,28 @@ def create_bin_root():
         ),
     )
 
+    choose_fish = py_trees_ros.actions.Service(
+        name = "Find out choice",
+        service_name = "/auv4/choice",
+        service_type=std_srvs.srv.Trigger,
+        request=std_srvs.srv.TriggerRequest(),
+        blackboard_key=fk("choice"),
+    )
+
+    set_choice = DynamicSetBlackboard(
+        name="Set Choice",
+        key="choice.success",
+        namespace=NAMESPACE,
+        update_key="position",
+        overwrite=True,
+        func=_make_selection,
+    )
+
     align_to_target = goto_node.FromBlackboard(
         name="Align to Target",
         parent_namespace=NAMESPACE,
-        pose_key="bin_pose",
+        pose_key="position",
     )
-
-    # For Yosie
-
-    # subscriber --> yaw (NED)
-
-    # rotation_pose = create_stamped_pose(
-    #     "auv4/base_link_ned",
-    #     0.0,  # Temporary, please update
-    #     0.0,
-    #     0.0,
-    #     0.0,
-    #     0.0,
-    #     yaw,
-    # )
-
-    # goto to rotation pose
-
-    # service call to /auv4/choice std_srvs::Trigger (py_trees API tells you how to do this)
-
-    # choose between offsets based on choice (see how torpedo does it)
-
-    # goto that offset
 
     bin_pose = create_stamped_pose(
         "Task03_DropBRUVS_optical/clustered",
@@ -151,7 +212,9 @@ def create_bin_root():
             enable_detections,
             enable_detections_succeeded,
             py_trees.timers.Timer("wait for match", duration=10.0),
-            align_to_target_const,
+            choose_fish,
+            set_choice,
+            align_to_target,
             py_trees.timers.Timer("wait to stabilize", duration=5.0),
             set_dropper_actuation,
             fire_dropper_1,
