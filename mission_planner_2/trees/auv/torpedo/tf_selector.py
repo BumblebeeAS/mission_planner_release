2145948bb87101648edf5f51998c9f4d409832fb
bbauv3 <bumblebeeauv@gmail.com>
2025-05-28 14:17:58 +0800

    refactor: rename tf selector behaviours

--- mission_planner_2/trees/auv/torpedo/tf_selector.py
+++ mission_planner_2/trees/auv/torpedo/tf_selector.py
@@ -5,8 +5,8 @@ import py_trees
 from mission_planner_2.commons import cache_tf
 from mission_planner_2.commons.pose_utils import create_stamped_pose
 
-FISH_HOLE = "auv4/torpedo_2/fish"
-SHARK_HOLE = "auv4/torpedo_2/shark"
+FISH_HOLE_FRAME = "auv4/torpedo_2/fish"
+SHARK_HOLE_FRAME = "auv4/torpedo_2/shark"
 
 
 def create_tf_selector_root(
@@ -17,36 +17,33 @@ def create_tf_selector_root(
 ) -> py_trees.composites.Selector:
     """
     Create the root node of the TF selector tree.
-
     Args:
         choice_key (str, optional): The full key for the choice blackboard variable. Defaults to "choice".
         pose_key (str, optional): The full key for the pose to fire at blackboard variable. Defaults to "pose".
         go_back_pose_key (str, optional): The full key for the go back pose blackboard variable. Defaults to "go_back_pose".
         is_first (bool, optional): Whether this is the first time the selector is being used. Defaults to True.
-
     Returns:
         py_trees.composites.Selector: The root node of the TF selector tree.
     """
-
     # Create the root node of the tree
-    root = py_trees.composites.Selector(
-        name="TF Selector Root",
+    sel_tf_root = py_trees.composites.Selector(
+        name="Tf selector root",
         memory=True,
     )
 
-    save_tf_fish_seq = py_trees.composites.Sequence(
-        name="Fish seq",
+    seq_fish_setup = py_trees.composites.Sequence(
+        name="Fish setup seq",
         memory=True,
     )
 
-    save_tf_shark_seq = py_trees.composites.Sequence(
-        name="Shark seq",
+    seq_shark_setup = py_trees.composites.Sequence(
+        name="Shark setup seq",
         memory=True,
     )
 
     # xor(1, 1)-> false xor(0, 1) -> true
     check_is_fish = py_trees.behaviours.CheckBlackboardVariableValue(
-        name="Check isFish",
+        name="Check is fish",
         check=py_trees.common.ComparisonExpression(
             variable=choice_key,
             value=False,
@@ -54,54 +51,54 @@ def create_tf_selector_root(
         ),
     )
 
-    save_tf_fish = cache_tf.ToBlackboard(
-        name="Save TF fish",
+    cache_tf_fish = cache_tf.ToBlackboard(
+        name="Cache tf fish",
         variable_name=go_back_pose_key,
-        start_frame="auv4/torpedo_2/fish",
-        end_frame="auv4/base_link_ned",
+        start=FISH_HOLE_FRAME,
+        end="auv4/base_link_ned",
     )
 
-    save_tf_shark = cache_tf.ToBlackboard(
-        name="Save TF Shark",
+    cache_tf_shark = cache_tf.ToBlackboard(
+        name="Cache tf shark",
         variable_name=go_back_pose_key,
-        start_frame="auv4/torpedo_2/shark",
-        end_frame="auv4/base_link_ned",
+        start=SHARK_HOLE_FRAME,
+        end="auv4/base_link_ned",
     )
 
     set_pose_fish = py_trees.behaviours.SetBlackboardVariable(
-        name="Set Pose Fish",
+        name="Set hole target pose",
         variable_name=pose_key,
-        variable_value=create_stamped_pose(FISH_HOLE),
+        variable_value=create_stamped_pose(FISH_HOLE_FRAME),
         overwrite=True,
     )
 
     set_pose_shark = py_trees.behaviours.SetBlackboardVariable(
-        name="Set Pose Shark",
+        name="Set hole target pose",
         variable_name=pose_key,
-        variable_value=create_stamped_pose(SHARK_HOLE),
+        variable_value=create_stamped_pose(SHARK_HOLE_FRAME),
         overwrite=True,
     )
 
-    save_tf_fish_seq.add_children(
+    seq_fish_setup.add_children(
         [
             check_is_fish,
-            save_tf_fish,
+            cache_tf_fish,
             set_pose_fish,
         ]
     )
 
-    save_tf_shark_seq.add_children(
+    seq_shark_setup.add_children(
         [
-            save_tf_shark,
+            cache_tf_shark,
             set_pose_shark,
         ]
     )
 
-    root.add_children(
+    sel_tf_root.add_children(
         [
-            save_tf_fish_seq,
-            save_tf_shark_seq,
+            seq_fish_setup,
+            seq_shark_setup,
         ]
     )
 
-    return root
+    return sel_tf_root
