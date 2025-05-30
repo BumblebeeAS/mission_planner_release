3a7ff71458457f62fc3e2529dc4f75358cb336a3
wesley <wesleywong08@gmail.com>
2025-05-30 18:45:54 +0800

    feat: upgraded dynamicsetbb for multi bb access

--- mission_planner_2/commons/blackboard.py
+++ mission_planner_2/commons/blackboard.py
@@ -35,20 +35,24 @@ class DynamicSetBlackboard(py_trees.behaviour.Behaviour):
         # Do something with args and kwargs
         return new_value
 
-    def func(x):
+    def func(x): # if one key passed in
         # Do something with curr_blackboard_value
         return your_custom_func(x, *args, **kwargs)
+
+    def func(x, y...): # if more than one key passed in
+        # Do something with curr_blackboard_value
+        return your_custom_func(x, y, ..., *args, **kwargs)
     ```
 
     Args:
         name (str): The name of the behaviour.
         namespace (str): The namespace of the blackboard variable.
-        key (str): The key of the blackboard variable to read from.
-        update_key (str): The key of the blackboard variable to write to (using the same key will overwrite the variable in the blackboard).
+        key (str | list[str]): The key[s] of the blackboard variable to read from.
+        update_key (str): The key of the blackboard variable to write to (using a key passed in will overwrite the value in the blackboard based on overwrite).
         overwrite (bool): Whether to overwrite the existing value in the blackboard.
-        func (callable): A unary function that takes the current value and returns the new value. Thhis function **MUST NOT**
+        func (callable): A N-ary function that takes the current value and returns the new value. This function **MUST NOT**
                          be blocking or throw errors. It **MUST** return the new value to be set in the blackboard.
-                         The function will be called with curr_blackboard_value retrieved with the key passed in.
+                         The function will be called with curr_blackboard_value[s] retrieved from the key[s] passed in.
     """
 
     def __init__(
@@ -61,30 +65,57 @@ class DynamicSetBlackboard(py_trees.behaviour.Behaviour):
         func=lambda x: x,
     ):
         super().__init__(name)
-        self.key = key
+        self.blackboard = self.attach_blackboard_client(
+            name="updater", namespace=namespace
+        )
         self.update_key = update_key
         self.func = func
         self.overwrite = overwrite
         self.namespace = namespace
-        self.blackboard = self.attach_blackboard_client(
-            name="updater", namespace=namespace
-        )
-        self.blackboard.register_key(
-            key=self.update_key,
-            access=py_trees.common.Access.WRITE,
-        )
-        self.blackboard.register_key(
-            key=self.key,
-            access=py_trees.common.Access.READ,
-        )
+        self.keys = self._process_keys(key)
+        self._register_keys(key, update_key)
 
     def update(self) -> py_trees.common.Status:
-        curr = self.blackboard.get(self.key)
         if self.blackboard.set(
             name=self.update_key,
-            value=self.func(curr),
+            value=self._apply(),
             overwrite=self.overwrite,
         ):
             return py_trees.common.Status.SUCCESS
 
         return py_trees.common.Status.FAILURE
+
+    def _process_keys(self, key: str | list[str]) -> list[str]:
+        """
+        Process the key or keys to ensure they are in a list format.
+        """
+        if isinstance(key, str):
+            return [key]
+        elif isinstance(key, list):
+            return key
+        else:
+            raise TypeError("Key must be a string or a list of strings.")
+
+    def _register_keys(self, keys: list[str], update_key: str) -> None:
+        """Register the keys in the blackboard.
+
+        Args:
+            keys (list[str]): list of keys to read from.
+            update_key (str): key to write to.
+        """
+        register = lambda k: self.blackboard.register_key(
+            key=k,
+            access=py_trees.common.Access.READ,
+        )
+
+        map(register, keys)
+        self.blackboard.register_key(
+            key=update_key,
+            access=py_trees.common.Access.WRITE,
+        )
+
+    def _apply(self) -> any:
+        """Apply the function to the current blackboard values."""
+        args = map(self.blackboard.get, self.keys)
+        out = self.func(*args)
+        return out
