820463c9a7f75818b60f374de61cbe37060868b8
advaypakhale <advay.pakhale@gmail.com>
2025-06-06 04:04:35 +0800

    test: add controls preemption test

---
+++ mission_planner_2/trees/auv/tests/test_preempt_controls.py
@@ -0,0 +1,48 @@
+import py_trees
+
+from mission_planner_2.commons.namespace_utils import (
+    full_key_generator,
+    generate_namespace,
+)
+from mission_planner_2.commons.pose_utils import create_stamped_pose
+from mission_planner_2.trees.auv.goto import goto
+
+# Define a main namespace for the test
+NAMESPACE = "/auv4/test_namespacing"
+NAMESPACE = generate_namespace()
+fk = full_key_generator(NAMESPACE)
+
+
+def f():
+    # Root sequence
+    first_pose = create_stamped_pose(
+        "auv4/base_link_ned", position_x=10.0, position_y=0.0, position_z=0.0
+    )
+
+    second_pose = create_stamped_pose(
+        "auv4/base_link_ned", position_x=0.0, position_y=-10.0, position_z=0.0
+    )
+    goto_second = goto.FromConstant("Second Goto", NAMESPACE, second_pose)
+    goto_first = goto.FromConstant("First Goto", NAMESPACE, first_pose)
+
+    timer_wait = py_trees.timers.Timer(
+        "Wait Timer",
+        3.0,  # Wait for 5 seconds
+    )
+
+    par = py_trees.composites.Parallel(
+        name="fallback",
+        policy=py_trees.common.ParallelPolicy.SuccessOnOne(),
+    )
+    fallback = py_trees.composites.Selector(name="fallbacl", memory=True)
+
+    par.add_children(
+        [
+            goto_first,
+            py_trees.decorators.SuccessIsFailure(name="invert", child=timer_wait),
+        ]
+    )
+
+    fallback.add_children([par, goto_second])
+
+    return fallback
