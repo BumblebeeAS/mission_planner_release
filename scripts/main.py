b5b631e805718edc5cfa77bb8413121296eed44d
yisiox <yisiong.y22@gmail.com>
2025-06-17 14:13:32 +0800

    Add rclpy shutdown to non interrupt branches

--- scripts/main.py
+++ scripts/main.py
@@ -36,10 +36,12 @@ def main():
         console.loginfo(console.yellow + "interrupted" + console.reset)
     except SystemExit:
         console.loginfo(console.yellow + "exiting" + console.reset)
+        rclpy.shutdown()
     except Exception as e:
         console.logfatal(
             console.red + "exception occurred: {}".format(e) + console.reset
         )
+        rclpy.shutdown()
     finally:
         console.loginfo(console.reset + "cleaning up")
         tree.shutdown()
