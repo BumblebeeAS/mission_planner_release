b605d25c86a300d4f23eb6d151289b6cb7ce8ab9
SamuelFoo <fooenzesamuel@gmail.com>
2025-07-20 22:40:44 +0800

    Reduce gate clustering duration

--- mission_planner_2/trees/auv/gate/gate.py
+++ mission_planner_2/trees/auv/gate/gate.py
@@ -28,7 +28,7 @@ fk = full_key_generator(NAMESPACE)
 ######################### UPDATE CONSTANTS HERE #########################
 VISION_SERVER_TOPIC = "/auv4/gate_front/manage_nodes"
 
-CLUSTERING_DURATION = 20
+CLUSTERING_DURATION = 4
 STABILIZE_DURATION = 7.0
 
 FORWARD_DISTANCE = 3.0
