3ee6a4510d1d152958a7917d38acbd8afcd789c1
advaypakhale <advay.pakhale@gmail.com>
2025-07-23 00:31:40 +0800

    refactor: modify main to use configured node

--- scripts/main.py
+++ scripts/main.py
@@ -2,10 +2,13 @@
 
 import py_trees
 import py_trees.console as console
+import py_trees_ros
 import rclpy
 
 from mission_planner_2.commons.hooks import stop_on_success_or_failure
-from mission_planner_2.commons.led_management import create_led_tree
+from mission_planner_2.commons.node_registry import TreeNode
+
+# from mission_planner_2.commons.led_management import create_led_tree
 from mission_planner_2.trees.auv.mother.mother import create_mother
 
 
@@ -13,7 +16,10 @@ def main():
     rclpy.init(args=None)
     root = create_mother()
     py_trees.logging.level = py_trees.logging.Level.DEBUG
-    tree, node = create_led_tree(root=root, display_only_visited_behaviours=True)
+    tree = py_trees_ros.trees.BehaviourTree(root=root, unicode_tree_debug=True)
+    tree.snapshot_visitor.display_only_visited_behaviours = True
+    node = TreeNode()
+    # tree, node = create_led_tree(root=root, display_only_visited_behaviours=True)
     try:
         tree.setup(node=node, timeout=60.0)
     except:
