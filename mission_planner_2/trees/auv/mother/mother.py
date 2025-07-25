520f42d19caf4e264309682b2c242de800a3b731
SamuelFoo <fooenzesamuel@gmail.com>
2025-07-25 13:20:14 +0800

    feat: updated move to task

--- mission_planner_2/trees/auv/mother/mother.py
+++ mission_planner_2/trees/auv/mother/mother.py
@@ -1,31 +1,31 @@
 import py_trees
+from geometry_msgs.msg import TransformStamped
+from tf_transformations import euler_from_quaternion
 
-from mission_planner_2.commons.pose_utils import compute_start_to_end_vector
+from mission_planner_2.commons.blackboard import DynamicSetBlackboard
+from mission_planner_2.commons.pose_utils import (
+    compute_start_to_end_vector,
+    create_stamped_pose,
+)
+from mission_planner_2.commons.tf_checker import create_tf_checker_from_constant_root
 from mission_planner_2.trees.auv.bins.bins import create_bin_root
-from mission_planner_2.trees.auv.bins.move_to_task import create_move_to_bin_task_root
 from mission_planner_2.trees.auv.button.wait_for_button import (
     create_wait_for_button_root,
 )
 from mission_planner_2.trees.auv.gate.gate import create_gate_root
 from mission_planner_2.trees.auv.gate.move_to_task import create_move_to_gate_task_root
 from mission_planner_2.trees.auv.gate.return_home import create_return_root
-from mission_planner_2.trees.auv.octagon.move_to_task import (
-    create_move_to_octagon_task_root,
-)
+from mission_planner_2.trees.auv.goto import goto
 from mission_planner_2.trees.auv.octagon.octagon import create_octagon_root
-from mission_planner_2.trees.auv.slalom.move_to_task import (
-    create_move_to_slalom_task_root,
-)
 from mission_planner_2.trees.auv.slalom.slalom import create_slalom_root
-from mission_planner_2.trees.auv.torpedo.move_to_task import (
-    create_move_to_torpedo_task_root,
-)
 from mission_planner_2.trees.auv.torpedo.torpedo import create_torpedo_root
 
 BUTTON_TOPIC = "/auv4/button/left"
 IS_LEFT_KEY = "/global/is_left_side"  # Global key for left option or not
 BASE_LINK_KEY = "/global/base_link"
 WORLD_KEY = "/global/world"
+CURRENT_ODOM_KEY = "/global/current_odom"
+ZERO_YAW_POSE_KEY = "/global/zero_yaw_pose_key"
 
 
 MAP_NED_COORDS_GATE_START = {
@@ -45,15 +45,15 @@ MAP_NED_COORDS_GATE_END = {  # TODO: MUST TUNE
     "yaw": 0.0,
 }
 MAP_NED_COORDS_SLALOM_START = {
-    "x": 7.0,
-    "y": -1.5,
+    "x": 5.0,
+    "y": 0.0,
     "z": 0.3,
     "roll": 0.0,
     "pitch": 0.0,
     "yaw": 0.0,
 }
 MAP_NED_COORDS_SLALOM_END = {  # TODO: MUST TUNE
-    "x": 13.0,
+    "x": 12.0,
     "y": -1.5,
     "z": 0.3,
     "roll": 0.0,
@@ -61,15 +61,15 @@ MAP_NED_COORDS_SLALOM_END = {  # TODO: MUST TUNE
     "yaw": 0.0,
 }
 MAP_NED_COORDS_BIN = {
-    "x": 16.0,
-    "y": 1.0,
+    "x": 15.0,
+    "y": -1.5,
     "z": 0.3,
     "roll": 0.0,
     "pitch": 0.0,
     "yaw": 0.0,
 }
 MAP_NED_COORDS_TORPEDO = {
-    "x": 16.0,
+    "x": 15.0,
     "y": 1.5,
     "z": 0.3,
     "roll": 0.0,
@@ -77,13 +77,91 @@ MAP_NED_COORDS_TORPEDO = {
     "yaw": 0.0,
 }
 MAP_NED_COORDS_OCTAGON = {
-    "x": 20.0,
-    "y": 1.5,
+    "x": 23.0,
+    "y": -1.0,
     "z": 0.3,
     "roll": 0.0,
     "pitch": 0.0,
     "yaw": 0.0,
 }
+
+
+def create_move_to_task(task: str, start: dict, end: dict, stabilise_time: float = 5.0):
+    root = py_trees.composites.Sequence(
+        name=f"Move to {task}",
+        memory=True,
+    )
+
+    get_odom = create_tf_checker_from_constant_root(
+        start_frames=["world_ned"],
+        end_frames=["auv4/base_link_ned"],
+        update_keys=[CURRENT_ODOM_KEY],
+        fallback_val=[None],
+    )
+
+    dynamic_create_zero_yaw_pose = DynamicSetBlackboard(
+        name="Set zero yaw pose",
+        key=CURRENT_ODOM_KEY,
+        update_key=ZERO_YAW_POSE_KEY,
+        overwrite=True,
+        func=get_zero_yaw_pose,
+    )
+
+    goto_zero_yaw = goto.FromBlackboard(
+        name="Goto zero yaw",
+        pose_key=ZERO_YAW_POSE_KEY,
+    )
+
+    timer_stabilise = py_trees.timers.Timer(
+        name="Stabilise",
+        duration=stabilise_time,
+    )
+
+    coords = compute_start_to_end_vector(start, end)
+
+    task_pose = create_stamped_pose(
+        "auv4/base_link_ned",
+        position_x=coords["x"],
+        position_y=coords["y"],
+        position_z=coords["z"],
+        roll=coords["roll"],
+        pitch=coords["pitch"],
+        yaw=coords["yaw"],
+    )
+
+    goto_task = goto.FromConstant(
+        name=f"Goto {task} start",
+        pose=task_pose,
+    )
+
+    root.add_children(
+        [
+            get_odom,
+            dynamic_create_zero_yaw_pose,
+            goto_zero_yaw,
+            timer_stabilise,
+            goto_task,
+        ]
+    )
+
+    return root
+
+
+def get_zero_yaw_pose(tf: TransformStamped):
+    _, _, y = euler_from_quaternion(
+        [
+            tf.transform.rotation.x,
+            tf.transform.rotation.y,
+            tf.transform.rotation.z,
+            tf.transform.rotation.w,
+        ]
+    )
+
+    return create_stamped_pose(
+        frame_id="auv4/base_link_ned",
+        yaw=-y,
+        use_radians=True,
+    )
 
 
 def create_mother():
@@ -114,28 +192,32 @@ def create_mother():
     gate_root = create_gate_root()
     move_to_gate = create_move_to_gate_task_root(MAP_NED_COORDS_GATE_START)
 
-    gate_to_slalom_vector = compute_start_to_end_vector(
-        MAP_NED_COORDS_GATE_END, MAP_NED_COORDS_SLALOM_START
+    move_to_slalom = create_move_to_task(
+        task="slalom",
+        start=MAP_NED_COORDS_GATE_END,
+        end=MAP_NED_COORDS_SLALOM_START,
     )
-    move_to_slalom = create_move_to_slalom_task_root(gate_to_slalom_vector)
     slalom_root = create_slalom_root()
 
-    slalom_to_bin_vector = compute_start_to_end_vector(
-        MAP_NED_COORDS_SLALOM_END, MAP_NED_COORDS_BIN
+    move_to_bin = create_move_to_task(
+        task="bin",
+        start=MAP_NED_COORDS_SLALOM_END,
+        end=MAP_NED_COORDS_BIN,
     )
-    move_to_bin = create_move_to_bin_task_root(slalom_to_bin_vector)
     bin_root = create_bin_root()
 
-    bin_to_torpedo_vector = compute_start_to_end_vector(
-        MAP_NED_COORDS_BIN, MAP_NED_COORDS_TORPEDO
+    move_to_torpedo = create_move_to_task(
+        task="torpedo",
+        start=MAP_NED_COORDS_BIN,
+        end=MAP_NED_COORDS_TORPEDO,
     )
-    move_to_torpedo = create_move_to_torpedo_task_root(bin_to_torpedo_vector)
     torpedo_root = create_torpedo_root()
 
-    torpedo_to_octagon_vector = compute_start_to_end_vector(
-        MAP_NED_COORDS_TORPEDO, MAP_NED_COORDS_OCTAGON
+    move_to_octagon = create_move_to_task(
+        task="octagon",
+        start=MAP_NED_COORDS_TORPEDO,
+        end=MAP_NED_COORDS_OCTAGON,
     )
-    move_to_octagon = create_move_to_octagon_task_root(torpedo_to_octagon_vector)
     octagon_root = create_octagon_root()
 
     # TODO: see if need a move to gate here to go closer to do the return task
@@ -153,18 +235,18 @@ def create_mother():
         [
             # wait_for_button,
             # set_is_left,
-            set_base_link_frame,
-            set_world_frame,  # TODO: use multi set bb?
-            move_to_gate,
-            gate_root,
-            move_to_slalom,
-            slalom_root,
-            move_to_bin,
-            bin_root,
-            move_to_torpedo,
-            torpedo_root,
+            # set_base_link_frame,
+            # set_world_frame,  # TODO: use multi set bb?
+            # move_to_gate,
+            # gate_root,
+            # move_to_slalom,
+            # slalom_root,
+            # move_to_bin,
+            # bin_root,
+            # move_to_torpedo,
+            # torpedo_root,
             # move_to_octagon,
-            # octagon_root,
+            octagon_root,
             # return_root,
         ]
     )
