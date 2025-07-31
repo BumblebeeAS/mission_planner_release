3dbdae5212f7b202bb8efcf0636b44461170b873
advaypakhale <advay.pakhale@gmail.com>
2025-07-31 18:11:23 +0800

    feat: update main to use led management

--- scripts/main.py
+++ scripts/main.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 
+import traceback
+
 import py_trees
 import py_trees.console as console
-import py_trees_ros
 import rclpy
 
 from mission_planner_2.commons.hooks import stop_on_success_or_failure
-
-# from mission_planner_2.commons.led_management import create_led_tree
-from mission_planner_2.commons.node_registry import TreeNode
-from mission_planner_2.trees.auv.mother.mother import create_mother
+from mission_planner_2.commons.led_management import create_led_tree
+from mission_planner_2.trees.auv.mother.mother import (
+    create_mother,
+    load_mission_coordinates,
+)
 
 
 def main():
     rclpy.init(args=None)
-    root = create_mother()
+    coords = load_mission_coordinates()
+    root = create_mother(coords)
     py_trees.logging.level = py_trees.logging.Level.DEBUG
-    tree = py_trees_ros.trees.BehaviourTree(root=root, unicode_tree_debug=True)
-    tree.snapshot_visitor.display_only_visited_behaviours = True
-    node = TreeNode()
-    # tree, node = create_led_tree(root=root, display_only_visited_behaviours=True)
+    tree, node = create_led_tree(root=root, display_only_visited_behaviours=True)
     try:
         tree.setup(node=node, timeout=60.0)
     except:
