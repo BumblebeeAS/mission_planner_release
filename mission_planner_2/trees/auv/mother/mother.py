867acc2c1e1f6455384f3ed0067ee760c81ca92d
bbauv3 <bumblebeeauv@gmail.com>
2025-08-11 13:39:33 +0800

    feat: buttons in mother

--- mission_planner_2/trees/auv/mother/mother.py
+++ mission_planner_2/trees/auv/mother/mother.py
@@ -4,8 +4,12 @@ import py_trees
 import py_trees_ros
 import yaml
 from ament_index_python.packages import get_package_share_directory
+from bb_controls_msgs.srv import Controller
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from robot_localization.srv import SetPose
 from std_srvs.srv import Trigger
 
+from mission_planner_2.commons import checked_service
 from mission_planner_2.trees.auv.bins.bins import create_bin_root
 from mission_planner_2.trees.auv.button.wait_for_button import (
     create_wait_for_button_root,
@@ -18,13 +22,16 @@ from mission_planner_2.trees.auv.octagon.octagon import create_octagon_root
 from mission_planner_2.trees.auv.slalom.slalom import create_slalom_root
 from mission_planner_2.trees.auv.torpedo.torpedo import create_torpedo_root
 
-BUTTON_TOPIC = "/auv4/button/left"
+LEFT_BUTTON_TOPIC = "/auv4/button/left"
+RIGHT_BUTTON_TOPIC = "/auv4/button/right"
 IS_LEFT_KEY = "/global/is_left_side"  # Global key for left option or not
 BASE_LINK_KEY = "/global/base_link"
 WORLD_KEY = "/global/world"
 CURRENT_ODOM_KEY = "/global/current_odom"
 ZERO_YAW_POSE_KEY = "/global/zero_yaw_pose_key"
 CHOICE_KEY = "/global/choice_is_fish"
+CONTROLS_SRV_TOPIC = "/auv4/controls/controller"
+RESET_POSE_SRV_TOPIC = "/auv4/nav/reset_pose"
 
 
 def load_mission_coordinates():
@@ -55,8 +62,41 @@ def create_mother(coords: dict):
         memory=True,
     )
 
-    wait_for_button = create_wait_for_button_root(
-        button_topic=BUTTON_TOPIC,
+    wait_for_left_button = create_wait_for_button_root(
+        button_topic=LEFT_BUTTON_TOPIC,
+        num_retries=1000000,
+    )
+
+    req = SetPose.Request()
+    req.pose = PoseWithCovarianceStamped()
+    req.pose.pose.pose.orientation.w = 1.0
+
+    srv_reset_pose = py_trees_ros.service_clients.FromConstant(
+        name="Reset pose",
+        service_type=SetPose,
+        service_name=RESET_POSE_SRV_TOPIC,
+        service_request=req,
+    )
+
+    srv_enable_controls = checked_service.FromConstant(
+        name="Enable controls",
+        service_name=CONTROLS_SRV_TOPIC,
+        service_type=Controller,
+        service_request=Controller.Request(
+            enable=True,
+            pause=False,
+            disable_altitude=False,
+        ),
+    )
+
+    retry_enable_controls = py_trees.decorators.Retry(
+        name="Retry enable controls",
+        child=srv_enable_controls,
+        num_failures=1000,
+    )
+
+    wait_for_right_button = create_wait_for_button_root(
+        button_topic=RIGHT_BUTTON_TOPIC,
         num_retries=1000000,
     )
 
@@ -127,7 +167,7 @@ def create_mother(coords: dict):
     )
     move_to_octagon = create_move_to_task(
         task="octagon",
-        start=coords["torpedo_post"],
+        start=coords["gate_end"],
         end=coords["octagon"],
         odom_key=CURRENT_ODOM_KEY,
         zero_yaw_pose_key=ZERO_YAW_POSE_KEY,
@@ -141,13 +181,16 @@ def create_mother(coords: dict):
     set_is_left = py_trees.behaviours.SetBlackboardVariable(
         name="Set is left for test",
         variable_name=IS_LEFT_KEY,
-        variable_value=True,
+        variable_value=False,
         overwrite=True,
     )
 
     root.add_children(
         [
-            # wait_for_button,
+            # wait_for_left_button,
+            # srv_reset_pose,
+            # retry_enable_controls,
+            # wait_for_right_button,
             srv_get_choice,
             # set_is_left,
             set_base_link_frame,
