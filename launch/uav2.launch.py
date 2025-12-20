79a6adfd9cf2e3c239c78eafb33930b2a2a339ae
SamuelFoo <fooenzesamuel@gmail.com>
2025-12-20 15:42:57 +0800

    Add uav2 launch to publish mission_tfs

---
+++ launch/uav2.launch.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    launch_objects = [
+        DeclareLaunchArgument(
+            "static_tf_file",
+            default_value=PathJoinSubstitution(
+                [
+                    FindPackageShare("mission_planner_2"),
+                    "cfg",
+                    "uav2",
+                    "static_tfs.yaml",
+                ]
+            ),
+        ),
+        DeclareLaunchArgument(
+            "dynamic_tf_file",
+            default_value="",
+        ),
+        DeclareLaunchArgument(
+            "default_suffix",
+            default_value="view",
+        ),
+    ]
+
+    nodes = [
+        Node(
+            package="mission_planner_2",
+            executable="mission_tfs_node.py",
+            name="mission_tfs_node",
+            parameters=[
+                {
+                    "static_tf_file": LaunchConfiguration("static_tf_file"),
+                    "dynamic_tf_file": LaunchConfiguration("dynamic_tf_file"),
+                    "default_suffix": LaunchConfiguration("default_suffix"),
+                }
+            ],
+            output="screen",
+        ),
+    ]
+
+    return LaunchDescription(launch_objects + nodes)
